61f7829f580cde1240364aa1c51758f50360838c
arliannasrul <arliannasrul@gmail.com>
2026-06-12 22:40:22 +0700

    enable error reporting for debugging

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Enable error reporting for debugging
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Forward request to Laravel public/index.php for Vercel Serverless Functions
 require __DIR__ . '/../public/index.php';
